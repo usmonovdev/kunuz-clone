2aff68c182bd67c4589316d793c21a29a2818af2
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-07 23:29:21 +0500

    Finish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
 import "./App.css"
 import All from "./components/All";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<All />} />
-    </Routes>
+    <All />
     </>
   )
 }
