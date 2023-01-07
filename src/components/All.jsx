55f8200fa59f1e54cae4348efb34f2c72c64d9e7
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-07 18:12:40 +0500

    Scroll to top button

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import Footer from './Footer'
-import Nav from './Nav'
-import TopNews from './TopNews'
+import React, { useEffect } from "react";
+import Footer from "./Footer";
+import Nav from "./Nav";
+import TopNews from "./TopNews";
+import { MdOutlineKeyboardArrowUp } from "react-icons/md";
+import { useState } from "react";
 
 function All() {
+  const [scroll, setScroll] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-    <Nav />
-    <TopNews />
-    <Footer />
+      <Nav />
+      <TopNews />
+      <Footer />
+      {scroll ? <div className="scroll" onClick={scrollToTop}><MdOutlineKeyboardArrowUp className="scroll-icon" /></div> : ""}
     </>
-  )
+  );
 }
 
-export default All
+export default All;
--- src/components/Recomendation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Recomendation() {
-  return (
-    <div>Recomendation</div>
-  )
-}
-
-export default Recomendation
